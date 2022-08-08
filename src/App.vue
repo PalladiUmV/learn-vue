fb773ef362049a5e4e31bb8c8d8b3dd42a210add
ImNotCryingOverYou <limited886@gmail.com>
2022-08-08 12:48:51 +0300

    first

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,57 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+
+	<div class="app">
+		<post-form @create="createPost" />
+		<post-list v-bind:posts="posts" />
+	</div>
+
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+<script> 
+import PostForm from './components/PostForm.vue'
+import PostList from './components/PostList.vue'
+
+export default {
+	components: {
+    PostForm,
+    PostList,
+},
+	data() {
+		return {
+			posts: [
+				{id: 1, title: 'JavaScript', body: 'Описание поста'},
+				{id: 2, title: 'JavaScript', body: 'Описание поста 2'},
+				{id: 3, title: 'JavaScript', body: 'Описание поста 3'},
+				{id: 4, title: 'JavaScript', body: 'Описание поста 4'},
+			],
+		}
+	},
+	methods: {
+		createPost(post) {
+			this.posts.push(post);
+		},
+	}
 }
 
-nav {
-  padding: 30px;
+</script>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+
+
+
+<style>
+
+* {
+	padding: 0;
+	margin: 0;
+	box-sizing: border-box;
+	font-family: 'Roboto';
+	font-size: 15px;
 }
+
+.app {
+	padding: 20px;
+}
+
 </style>
